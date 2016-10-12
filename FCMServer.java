5a4ae244a1985bbfc98ec32842ebdd504a071729
jas999 <jaspreet605@gmail.com>
2016-10-12 13:48:41 +0530

    Update FCMServer.java

--- FCMServer.java
+++ FCMServer.java
@@ -1,11 +1,6 @@
 package com.firebase.notifications;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Iterator;
 
 import javax.servlet.ServletContextEvent;
@@ -21,14 +16,18 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
+/**
+ * This class will be initialized on startup. This is working as a Listener, to listen any changes on Firebase Database
+ * @author Jaspreet Singh
+ *
+ */
 public class FCMServer implements ServletContextListener {
 
 	private static final String DBNAME = "https://dgc-connect.firebaseio.com";
-	private static final String SERVICE_ACCOUNT_KEY_FILE_PATH = "DGC_Connect-bfca9e40d132.json";
-	private static final String NOTIFICATION_SERVER_URL="https://fcm.googleapis.com/fcm/send";
-	private static final String API_KEY  = "AIzaSyBASg2VWvqn4N4WS-ow6VCrBhuPo_2XvC8";
-	private static final String PUSH_MESSAGES_NODE = "push_messages/chat";
+	private String SERVICE_ACCOUNT_KEY_FILE_PATH = "DGC_Connect-bfca9e40d132.json";	
+	private String PUSH_MESSAGES_NODE = "push_messages/chat";
+	private String MESSAGE_NODE = "message";
+	private String DEVICE_NODE = "deviceid";
 	private static final Logger logger = Logger.getLogger(FCMServer.class);
 	
 	public void contextDestroyed(ServletContextEvent servletContextListener) {	
@@ -37,20 +36,27 @@ public class FCMServer implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent servletContextListener) {
 		logger.debug("==========Initializing Firebase============");
-		initializeFirebaseDatabase();
-		
+		initializeFirebaseDatabase();		
 	}
 	
 	public void initializeFirebaseDatabase(){
 		logger.debug("==========Goint to Initializing Firebase Server============");
 		logger.debug("Firebase database : " +  DBNAME);
-		logger.debug("Service Account Key JSON File Path : " + SERVICE_ACCOUNT_KEY_FILE_PATH );
 		try {
+			
+			SERVICE_ACCOUNT_KEY_FILE_PATH = PropertyFileLoader.getPropertiesInstance().getProperty("fcm.JSON_FILE_PATH");
+			logger.debug("SERVICE_ACCOUNT_KEY_FILE_PATH : " + SERVICE_ACCOUNT_KEY_FILE_PATH);
 			// Initialize the app with a service account, granting admin
 			// privileges
 			FirebaseOptions options = new FirebaseOptions.Builder().setDatabaseUrl(DBNAME)
 					.setServiceAccount(new FileInputStream(SERVICE_ACCOUNT_KEY_FILE_PATH)).build();
 			FirebaseApp.initializeApp(options);
+			
+			
+			PUSH_MESSAGES_NODE = PropertyFileLoader.getPropertiesInstance().getProperty("fcm.PUSH_MESSAGES_NODE");
+			
+			MESSAGE_NODE = PropertyFileLoader.getPropertiesInstance().getProperty("fcm.MESSAGE_NODE");
+			DEVICE_NODE = PropertyFileLoader.getPropertiesInstance().getProperty("fcm.DEVICE_NODE");
 			logger.debug("PUSH Messages Node : " + PUSH_MESSAGES_NODE );
 			// As an admin, the app has access to read and write all data,
 			// regardless of Security Rules
@@ -59,8 +65,8 @@ public class FCMServer implements ServletContextListener {
 				
 				public void onChildRemoved(DataSnapshot dataSnapshot) {
 					Object document = dataSnapshot.getValue();
-					System.out.println(document);
-					System.out.println("=========ON Child Removed=======");
+					logger.debug(document);
+					logger.debug("=========ON Child Removed=======");
 
 				}
 				
@@ -69,9 +75,9 @@ public class FCMServer implements ServletContextListener {
 					System.out.println(document);
 					JSONObject json = new JSONObject(document.toString());
 					
-					logger.debug("message " + json.get("message"));
-					logger.debug("device " + json.get("deviceid"));
-					System.out.println("=========ON Child Changed=======");
+					logger.debug("message " + json.get(MESSAGE_NODE));
+					logger.debug("device " + json.get(DEVICE_NODE));
+					logger.debug("=========ON Child Changed=======");
 
 				}
 				
@@ -87,19 +93,15 @@ public class FCMServer implements ServletContextListener {
 								DataSnapshot snapShot = itr.next();
 								logger.debug("ITR Key : " + snapShot.getKey());
 								logger.debug("ITR Value : " + snapShot.getValue());
-								if(snapShot.getKey().equalsIgnoreCase("deviceid")){
+								if(snapShot.getKey().equalsIgnoreCase(DEVICE_NODE)){
 									 deviceId = snapShot.getValue().toString();
 									
 								}
-								if(snapShot.getKey().equals("message")){
+								if(snapShot.getKey().equals(MESSAGE_NODE)){
 									message = snapShot.getValue().toString();
 								}
-								
-								
-								 
 							}
-							FCMNotifications notification = new FCMNotifications();
-							
+							FCMNotifications notification = new FCMNotifications();							
 							String response = notification.sendNotifications(deviceId, message);
 							if(response != null && !response.equals("")){
 								Integer success = processResponse(response);
@@ -107,78 +109,26 @@ public class FCMServer implements ServletContextListener {
 								logger.debug(success == 1 ? "Message Sent." : "Failed");
 								dataSnapshot.getRef().removeValue();
 							}
-							
-							
 						}
-						
-						
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
-					
-					
-					
 				}
+
 				public void onCancelled(DatabaseError arg0) {
 					// TODO Auto-generated method stub
 					
 				}
+
 				public void onChildMoved(DataSnapshot arg0, String arg1) {
 					// TODO Auto-generated method stub
 					
 				}
-				
 			});
 						
 		} catch (Exception e) {
 			e.printStackTrace();
-			
 		}
-		
-	}
-	
-	private void sendNotifications() {
-
-		try {
-			
-			URL url = new URL(NOTIFICATION_SERVER_URL);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
-
-			conn.setRequestProperty("Authorization", "key=" + API_KEY);
-
-			conn.setDoOutput(true);
-
-			String input = "{\"to\" : [\"Specify token you got from GCM\"],\"data\" : {\"message\": \"hi  welcome\"},}";
-
-			OutputStream os = conn.getOutputStream();
-			os.write(input.getBytes());
-			os.flush();
-			os.close();
-			
-			int responseCode = conn.getResponseCode();
-			System.out.println("\nSending 'POST' request to URL : " + url);
-			System.out.println("Post parameters : " + input);
-			System.out.println("Response Code : " + responseCode);
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-
-			// print result
-			System.out.println(response.toString());
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
 	}
 	public static void main(String[] args){
 		//FCMServer server = new FCMServer();
@@ -190,6 +140,11 @@ public class FCMServer implements ServletContextListener {
 		System.out.println(json.get("success"));
 	}
 	
+	/**
+	 * To process the response returned from the server
+	 * @param response
+	 * @return
+	 */
 	private Integer processResponse(String response){
 		logger.debug("Going to processResponse" + response);
 		Integer success = 0 ;
@@ -202,7 +157,5 @@ public class FCMServer implements ServletContextListener {
 		return success;
 		
 	}
-	
-	
 
 }
